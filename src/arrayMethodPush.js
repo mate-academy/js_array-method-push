40e6fd8ae64d94aa2bc0451e940d22c74246e0f1
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 18:17:29 +0300

    updated eslint config. Fixed eslint errors.

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -6,8 +6,8 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
  */
 
-function applyCustomPush () {
-  [].__proto__.push2 = function () {
+function applyCustomPush() {
+  [].__proto__.push2 = function() {
     // write code here
   };
 }
