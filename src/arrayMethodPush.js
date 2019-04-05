6e301294b06c70dbdab29bf886f7b98b5378c900
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-05 13:52:25 +0300

    removed empty line after doc, changed typeof checking to .toBeinstanceOf, moved expectation checks to new line, removed package-lock.json from .gitignore

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -3,9 +3,7 @@
 /**
  * Implement method push
  *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
  */
-
 function applyCustomPush() {
   [].__proto__.push2 = function() {
     // write code here
--- src/arrayMethodPush.test.js
+++ src/arrayMethodPush.test.js
@@ -4,33 +4,42 @@ const applyCustomPush = require('./arrayMethodPush');
 applyCustomPush();
 
 test('`push2` is added to [].__proto__', () => {
-  expect(typeof [].push2).toBe('function');
+  expect([].push2)
+    .toBeInstanceOf(Function);
 });
 
 test('push single item', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2(4);
-  expect(source).toEqual([0, 1, 2, 3, 4]);
-  expect(result).toBe(5);
+  expect(source)
+    .toEqual([0, 1, 2, 3, 4]);
+  expect(result)
+    .toBe(5);
 });
 
 test('push multiple items', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2(4, 5, 6);
-  expect(source).toEqual([0, 1, 2, 3, 4, 5, 6]);
-  expect(result).toBe(7);
+  expect(source)
+    .toEqual([0, 1, 2, 3, 4, 5, 6]);
+  expect(result)
+    .toBe(7);
 });
 
 test('push no items', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2();
-  expect(source).toEqual([0, 1, 2, 3]);
-  expect(result).toBe(4);
+  expect(source)
+    .toEqual([0, 1, 2, 3]);
+  expect(result)
+    .toBe(4);
 });
 
 test('push an array', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2([4, 5, 6]);
-  expect(source).toEqual([0, 1, 2, 3, [4, 5, 6]]);
-  expect(result).toBe(5);
+  expect(source)
+    .toEqual([0, 1, 2, 3, [4, 5, 6]]);
+  expect(result)
+    .toBe(5);
 });
