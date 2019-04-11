8a2af7df0195aca3538808fa3cf1b496f422e9c8
Denis <krollik1000000@gmail.com>
2019-04-11 12:22:33 +0300

    'denisSkab'

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -4,9 +4,11 @@
  * Implement method push
  *
  */
+
 function applyCustomPush() {
-  [].__proto__.push2 = function() {
-    // write code here
+  [].__proto__.push2 = function(...args) {
+    this.splice(this.length, 0, ...args);
+    return this.length;
   };
 }
 
