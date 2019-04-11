8d84e2649eb0a743e902c37cffcca7748844efde
Alexandr <alexandrmail32@gmail.com>
2019-04-11 11:42:11 +0300

    Solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -6,14 +6,10 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function() {
-    if (arguments.length) {
-      for (var i = 0; i < arguments.length; i++) {
-        this[this.length] = arguments[i];
-      }
-      return this.length;
-    } else {
-      return this.length;
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
     }
+    return this.length;
   };
 }
 
