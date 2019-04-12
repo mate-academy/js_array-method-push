c3a693285d295d918109a4bd8a9c7932e74732ad
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-13 00:36:26 +0300

    changed arguments name

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -4,7 +4,7 @@
  * Implement method push
  */
 function applyCustomPush() {
-  [].__proto__.push2 = function(...args) {
+  [].__proto__.push2 = function(...elements) {
     // write code here
   };
 }
