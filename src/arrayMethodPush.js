17eba760246113633598afda86175b218f246abd
Dmitry Batsenko <batsenkod@gmail.com>
2019-04-13 12:25:26 +0300

    solution_batsenko

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,8 +5,10 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    for (let arg = 0; arg < elements.length; arg++) {
+      this[this.length] = elements[arg];
+    }
+    return this.length;
   };
 }
-
 module.exports = applyCustomPush;
