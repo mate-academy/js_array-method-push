8331926c809d9cf365fdaee9242a941d290d898a
yojikyo <yojikyo@gmail.com>
2019-04-14 20:39:12 +0300

    'solution'

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -6,6 +6,10 @@
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
     // write code here
+    elements.forEach(element => {
+      this[this.length] = element;
+    });
+    return this.length;
   };
 }
 
