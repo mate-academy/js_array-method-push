1e14a20b111700f54a73c5c400578a380ccab930
Ivan Deiko <ivandeiko1997@gmail.com>
2019-04-25 15:34:50 +0300

    right?

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -6,8 +6,8 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...args) {
-    for (let i = 0; i < args.length; i++) {
-      this[this.length] = args[i];
+    for (let value of args) {
+      this[this.length] = value;
     }
     return this.length;
   };
