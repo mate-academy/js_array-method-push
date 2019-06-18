17d097e6d4fa5234381e98f6b07e9364fbecd02d
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-06-18 23:21:08 +0300

    'Solution'

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,10 +5,7 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    for (const element of elements) {
-      this[this.length] = element;
-    }
-    return this.length;
+
   };
 }
 
