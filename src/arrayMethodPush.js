e95308363484ee72dd80959e1404d2997e93e70c
neitiris <veliantos@gmail.com>
2019-06-19 14:48:51 +0300

    Solution commit

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -6,6 +6,11 @@
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
     // write code here
+    const len = this.length;
+    for (const element of elements) {
+      this[len] = element;
+    }
+    return len;
   };
 }
 
