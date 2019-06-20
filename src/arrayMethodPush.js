7d3842cfc21b9694746216f09632b9d68a1ca809
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-06-20 15:45:41 +0300

    Solution
    Solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -1,12 +1,13 @@
 'use strict';
-
 /**
  * Implement method push
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    for (const item of elements) {
+      this[this.length] = item;
+    }
+    return this.length;
   };
-}
-
+};
 module.exports = applyCustomPush;
