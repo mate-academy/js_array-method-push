c8389e18b85635da42c793e29868e48bd18d84f0
vladyslav223 <vladyslav223@gmail.com>
2019-08-16 20:25:01 +0300

    done

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -6,6 +6,13 @@
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
     // write code here
+    const arr = elements;
+    if (this.length) {
+      for (let i = 0; i < arr.length; i++) {
+        this[this.length] = elements[i];
+      }
+    }
+    return this.length ? this.length : undefined;
   };
 }
 
