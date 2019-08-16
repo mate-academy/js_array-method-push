1b0cdffcb78d56d5e1df76d031f891f1dde6444d
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-16 16:54:34 +0300

    refactoring

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,16 +5,11 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    let length = 0;
-    // eslint-disable-next-line no-unused-vars
-    for (const item of this) {
-      length++;
-    }
     for (const element of elements) {
-      this[length++] = element;
+      this[this.length] = element;
     }
 
-    return length;
+    return this.length;
   };
 }
 
