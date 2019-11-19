1eb2fee8299a6635d618c5a14e273382137db674
IsaacKlark <isaackeklarke@gmail.com>
2019-11-19 23:02:40 +0200

    solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    let j = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      this[i + this.length - j] = elements[i];
+      j++;
+    }
+
+    return this.length;
   };
 }
 
