58fa1edd4cf4adc85157f9daab95e57fd5fe56cd
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-25 21:57:22 +0200

    add solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,16 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    const arrElements = [...elements];
+    const arrLength = this.length;
+
+    for (let i = 0; i < arrElements.length; i++) {
+      const addIndex = i + arrLength;
+
+      this[addIndex] = arrElements[i];
+    }
+
+    return this.length;
   };
 }
 
