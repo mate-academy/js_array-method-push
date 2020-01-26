3cacf225e529163e3aa1d32fa76d5156e72c04d2
masha <mariafil2107@gmail.com>
2020-01-26 14:11:13 +0200

    push

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    const argLength = elements.length;
+
+    for (let i = 0; i < argLength; i++) {
+      this[this.length] = elements[i];
+    }
+
+    return this.length;
   };
 }
 
