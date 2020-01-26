a626f9db03004298417b2edca63714870885e4f5
Senya Volkov <formobileprima@gmail.com>
2020-01-27 00:14:07 +0200

    solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,14 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    const mainArr = Array.from(this);
+    const result = this;
+
+    for (let i = 0; i < elements.length; i++) {
+      result[i + mainArr.length] = elements[i];
+    }
+
+    return result.length;
   };
 }
 
