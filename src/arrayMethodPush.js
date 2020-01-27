2ad825dbd4cf6783a15f36cfbb57cc8aa009db0d
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-27 16:35:16 +0200

    delete extra variables

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,13 +5,8 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    const arrElements = [...elements];
-    const arrLength = this.length;
-
-    for (let i = 0; i < arrElements.length; i++) {
-      const addIndex = i + arrLength;
-
-      this[addIndex] = arrElements[i];
+    for (let i = 0; i < elements.length; i++) {
+      this[this.length] = elements[i];
     }
 
     return this.length;
