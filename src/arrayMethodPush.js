512207313d33e2400c62da81212732d57c879723
dima <tynindima@gmail.com>
2020-01-27 17:46:51 +0200

    Solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,11 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    for (const index of elements) {
+      this[this.length] = index;
+    }
+
+    return this.length;
   };
 }
 
