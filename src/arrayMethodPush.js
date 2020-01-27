8affeddcec54de6b067938aae0a410302d50c963
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-27 13:01:19 +0200

    delete items

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,11 +5,9 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    const items = elements;
-
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
       this.length += 1;
-      this[this.length - 1] = items[i];
+      this[this.length - 1] = elements[i];
     }
 
     return this.length;
