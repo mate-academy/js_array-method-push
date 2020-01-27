efe2a9549d463abc661bd209369580a7cb09e3fe
dima <tynindima@gmail.com>
2020-01-27 18:59:58 +0200

    Recall index to element

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,8 +5,8 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    for (const index of elements) {
-      this[this.length] = index;
+    for (const element of elements) {
+      this[this.length] = element;
     }
 
     return this.length;
