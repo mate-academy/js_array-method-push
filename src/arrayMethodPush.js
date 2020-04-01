b39220ce06b1e89cf8f609c25bf26a9a50804dc3
Andrey Faychak <faychak333@gmail.com>
2020-04-01 17:25:59 +0300

    'solution'

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -4,8 +4,15 @@
  * Implement method push
  */
 function applyCustomPush() {
-  [].__proto__.push2 = function(...elements) {
+  [1].__proto__.push2 = function(...elements) {
     // write code here
+    const myLength = this.length;
+
+    for (let i = 0; i < arguments.length; i++) {
+      this[i + myLength] = arguments[i];
+    }
+
+    return this.length;
   };
 }
 
