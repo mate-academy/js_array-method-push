998ab491c970b515a05faf60c8d1c8d25bace0d1
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-04-01 15:20:24 +0300

    solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -4,9 +4,15 @@
  * Implement method push
  */
 function applyCustomPush() {
-  [].__proto__.push2 = function(...elements) {
-    // write code here
+  [].__proto__.push2 = function (...elements) {
+    for (let element of elements) {
+      this[this.length] = element;
+    }
+    
+    return this.length;
   };
 }
 
 module.exports = applyCustomPush;
+
+// Вызов fruits.push(...) равнозначен fruits[fruits.length] = ....
