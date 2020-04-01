8f212f19ffb1df173b551a983b88268d40149468
Vitalii Shytikov <v.shytikov@gmail.com>
2020-04-01 15:07:31 +0300

    deleted variables

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,15 +5,11 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    const pushed = this;
-    const pushedLength = pushed.length;
-    const toAdd = [...elements];
-
-    for (let i = 0; i < toAdd.length; i++) {
-      pushed[pushedLength + i] = toAdd[i];
+    for (let i = 0; i < elements.length; i++) {
+      this[this.length] = elements[i];
     }
 
-    return pushed.length;
+    return this.length;
   };
 }
 
