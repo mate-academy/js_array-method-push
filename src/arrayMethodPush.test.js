dbdfac58e6102e78fdd8fca2b242d0f4e040df88
Yevhenii <DrozhzhynY>
2020-04-01 16:49:46 +0300

    solution2

--- src/arrayMethodPush.test.js
+++ src/arrayMethodPush.test.js
@@ -1,8 +1,7 @@
-/*eslint-disable*/
-
 'use strict';
 
 const applyCustomPush = require('./arrayMethodPush');
+
 applyCustomPush();
 
 test('push2 is added to [].__proto__', () => {
@@ -18,8 +17,10 @@ test(`push2 doesn't call default push`, () => {
 test('push single item', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2(4);
+
   expect(source)
     .toEqual([0, 1, 2, 3, 4]);
+
   expect(result)
     .toBe(5);
 });
@@ -27,8 +28,10 @@ test('push single item', () => {
 test('push multiple items', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2(4, 5, 6);
+
   expect(source)
     .toEqual([0, 1, 2, 3, 4, 5, 6]);
+
   expect(result)
     .toBe(7);
 });
@@ -36,8 +39,10 @@ test('push multiple items', () => {
 test('push no items', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2();
+
   expect(source)
     .toEqual([0, 1, 2, 3]);
+
   expect(result)
     .toBe(4);
 });
@@ -45,8 +50,10 @@ test('push no items', () => {
 test('push an array', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2([4, 5, 6]);
+
   expect(source)
     .toEqual([0, 1, 2, 3, [4, 5, 6]]);
+
   expect(result)
     .toBe(5);
 });
