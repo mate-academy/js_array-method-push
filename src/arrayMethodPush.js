871616fe1dfe9e99af56c6647923c54eab904207
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-04-01 15:38:17 +0300

    solution

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,20 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    const element = elements;
+    let pushLenght = this.reverse();
+
+    for (let i = 0; i < element.length; i++) {
+      pushLenght = this.unshift(element[i]);
+    }
+
+    this.reverse();
+
+    if (element.length === 0) {
+      return pushLenght.length;
+    }
+
+    return pushLenght;
   };
 }
 
