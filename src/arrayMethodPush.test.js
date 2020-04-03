fad1e876a53bb0c27007b22bc7df2150163b5ac6
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 17:45:08 +0300

    test.js lint ERROR was fixed...

--- src/arrayMethodPush.test.js
+++ src/arrayMethodPush.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const applyCustomPush = require('./arrayMethodPush');
+
 applyCustomPush();
 
 test('push2 is added to [].__proto__', () => {
@@ -16,8 +17,10 @@ test(`push2 doesn't call default push`, () => {
 test('push single item', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2(4);
+
   expect(source)
     .toEqual([0, 1, 2, 3, 4]);
+
   expect(result)
     .toBe(5);
 });
@@ -25,26 +28,37 @@ test('push single item', () => {
 test('push multiple items', () => {
   const source = [0, 1, 2, 3];
   const result = source.push2(4, 5, 6);
+
   expect(source)
+
     .toEqual([0, 1, 2, 3, 4, 5, 6]);
+
   expect(result)
     .toBe(7);
 });
 
 test('push no items', () => {
   const source = [0, 1, 2, 3];
+
   const result = source.push2();
+
   expect(source)
+
     .toEqual([0, 1, 2, 3]);
+
   expect(result)
     .toBe(4);
 });
 
 test('push an array', () => {
   const source = [0, 1, 2, 3];
+
   const result = source.push2([4, 5, 6]);
+
   expect(source)
+
     .toEqual([0, 1, 2, 3, [4, 5, 6]]);
+
   expect(result)
     .toBe(5);
 });
