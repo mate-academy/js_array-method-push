9d487d59f9cf5f8c78d724de9cc4597453d31828
tyooma <flying.alien.godzilla@gmail.com>
2020-05-29 18:02:31 +0300

    Array Push

--- src/arrayMethodPush.js
+++ src/arrayMethodPush.js
@@ -5,7 +5,15 @@
  */
 function applyCustomPush() {
   [].__proto__.push2 = function(...elements) {
-    // write code here
+    const pushedArrLength = arguments.length + this.length;
+
+    const j = this.length;
+
+    for (let i = this.length; i < pushedArrLength; i++) {
+      this[i] = elements[i - j];
+    }
+
+    return pushedArrLength;
   };
 }
 
